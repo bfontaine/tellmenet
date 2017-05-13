1aea8f2c02c627eef0a00dd52899ec850445f027
Baptiste Fontaine <b@ptistefontaine.fr>
2017-05-13 11:33:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# tel[l me ]net
+
+`tellmenet` is an experimental TCP server that writes everything it knows about
+a connection when it’s opened, then close it.
+
+It is intended to be used with `telnet`:
+
+    $ telnet <yourdomain>
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	var bindAddress = flag.String("bind", "0.0.0.0", "bind address")
+	var bindPort = flag.Int("port", 23, "port")
+
+	flag.Parse()
+
+	bind := fmt.Sprintf("%s:%d", *bindAddress, *bindPort)
+
+	l, err := net.Listen("tcp", bind)
+	if err != nil {
+		log.Fatalf("Cannot bind on %s: %v", bind, err)
+	}
+
+	defer l.Close()
+	log.Printf("Listening on %s", bind)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Can't accept: %v", err)
+		}
+
+		log.Printf("Someone's connected: %v", conn.RemoteAddr())
+
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	conn.Write(connInfos(conn))
+	conn.Close()
+}
+
+func connInfos(conn net.Conn) []byte {
+	var buff bytes.Buffer
+
+	writeField := func(name, value string) {
+		buff.Write([]byte(fmt.Sprintf("%s: %s\n", name, value)))
+	}
+
+	remote := conn.RemoteAddr()
+	if host, port, err := net.SplitHostPort(remote.String()); err == nil {
+		writeField("Remote IP", host)
+		writeField("Remote Port", port)
+
+		if names, err := net.LookupAddr(host); err == nil && len(names) > 0 {
+			if len(names) == 1 {
+				writeField("Remote Name", names[0])
+			}
+		}
+	}
+
+	return buff.Bytes()
+}
